0fe16eb1111ad3f7e303ba1fc54af02cdb921afa
David Anderson <leakeddave@me.com>
2012-09-19 15:18:23 -0700

    fix typos

--- src/com/applovin/sdkdemo/CustomTargetingActivity.java
+++ src/com/applovin/sdkdemo/CustomTargetingActivity.java
@@ -44,15 +44,15 @@ public class CustomTargetingActivity
         // Set phone number of the current user.
         //
         // Please note: Raw phone number will not be transferred over the
-        // network. AppLovin SDK will hash the value before using it.
+        // network. AppLovin SDK will hash the value before sending it.
         //
         targeting.setPhoneNumber("+1-555-555-1234");
 
         //
-        // Set an email of the current user.
+        // Set email of the current user.
         //
         // Please note: Raw value will not be transferred over the
-        // network. AppLovin SDK will hash the value before using it.
+        // network. AppLovin SDK will hash the value before sending it.
         //
         targeting.setEmail("test@gmail.com");
 
@@ -62,47 +62,47 @@ public class CustomTargetingActivity
         targeting.setEmails("test@gmail.com", "test@yahoo.com", "test@live.com");
 
         //
-        // Set the first name of current user.
+        // Set the first name of the current user.
         //
         targeting.setFirstName("Bob");
 
         //
-        // Set the year of birth of current user.
+        // Set the year of birth of the current user.
         //
         targeting.setBirthYear(1978);
 
         //
-        // Set the gender of current user. Use GENDER_MALE and GENDER_FEMALE
+        // Set the gender of the current user. Use GENDER_MALE and GENDER_FEMALE
         // constants of this class or pass 'm' or 'f' directly.
         //
         targeting.setGender(AppLovinTargetingData.GENDER_MALE);
 
         //
-        // Set highest level of education of current user. Supported education values are
-        // defined in following constants: EDUCATION_NONE, EDUCATION_HIGH_SCHOOL, 
+        // Set highest level of education for the current user. Supported education values are
+        // defined in the following constants: EDUCATION_NONE, EDUCATION_HIGH_SCHOOL, 
         // EDUCATION_SOME_COLLEGE, EDUCATION_IN_COLLEGE, EDUCATION_BACHELORS_DEGREE, 
         // EDUCATION_MASTERS_DEGREE, EDUCATION_DOCTORAL_DEGREE and EDUCATION_OTHER
         //
         targeting.setEducation( AppLovinTargetingData.EDUCATION_BACHELORS );
         
         //
-        // Set martital status of the current users. Supported values are
-        // defined in following constants: MARITAL_STATUS_NONE, 
+        // Set the marital status of the current user. Supported values are
+        // defined in the following constants: MARITAL_STATUS_NONE, 
         // MARITAL_STATUS_SINGLE, MARITAL_STATUS_MARRIED, 
         // MARITAL_STATUS_DIVORCED and MARITAL_STATUS_WIDOWED
         //
         targeting.setMaritalStatus( AppLovinTargetingData.MARITAL_STATUS_SINGLE );
         
         //
-        // Set ethnicity of the current user. Suppored values are
-        // defined in following constants: ETHNICITY_NONE, ETHNICITY_MIXED, 
+        // Set ethnicity of the current user. Supported values are
+        // defined in the following constants: ETHNICITY_NONE, ETHNICITY_MIXED, 
         // ETHNICITY_ASIAN, ETHNICITY_BLACK, ETHNICITY_HISPANIC, 
         // ETHNICITY_NATIVE_AMERICAN, ETHNICITY_WHITE and ETHNICITY_OTHER.
         //
         targeting.setEthnicity( AppLovinTargetingData.ETHNICITY_MIXED );
         
         //
-        // Set user's income. Income format is could be as follows:
+        // Set the user's income. Income format can be as follows:
         //   - An exact number like 100000 or 100K
         //   - A range like 10K-30K
         //   - A range like <100K
@@ -110,13 +110,13 @@ public class CustomTargetingActivity
         targeting.setIncome("80000-120K");
         
         //
-        // Set the location of current user.
+        // Set the location of the current user.
         //
         Location location = new Location("user");
         targeting.setLocation(location);
 
         //
-        // Set the language of current user. Language is expressed as
+        // Set the language of the current user. Language is expressed as
         // two-character ISO 639-1 language code.
         //
         targeting.setLanguage("RU");
@@ -132,12 +132,12 @@ public class CustomTargetingActivity
         targeting.setInterests("books", "games", "pizza");
 
         //
-        // Put an extra targeting parameter
+        // Put an extra targeting parameter.
         //
         targeting.putExtra("some_key", "some_value");
 
         //
-        // Clear all saved targeting data
+        // Clear all saved targeting data.
         //
 
         // targeting.clearData();
