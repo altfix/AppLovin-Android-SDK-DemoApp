cecfb952e540798cf7ef266ff8ea5c06994c9b93
David Anderson <leakeddave@me.com>
2013-01-16 15:45:13 -0800

    Add AdMobMediationInterEvent

--- src/com/applovin/sdkdemo/AdMobMediationInterEvent.java
+++ src/com/applovin/sdkdemo/AdMobMediationInterEvent.java
@@ -3,8 +3,11 @@ package com.applovin.sdkdemo;
 import android.app.Activity;
 
 import com.applovin.adview.AppLovinAdView;
+import com.applovin.adview.AppLovinInterstitialAd;
+import com.applovin.adview.AppLovinInterstitialAdDialog;
 import com.applovin.sdk.AppLovinAd;
 import com.applovin.sdk.AppLovinAdClickListener;
+import com.applovin.sdk.AppLovinAdDisplayListener;
 import com.applovin.sdk.AppLovinAdLoadListener;
 import com.applovin.sdk.AppLovinAdSize;
 import com.applovin.sdk.AppLovinSdk;
@@ -12,6 +15,8 @@ import com.google.ads.AdSize;
 import com.google.ads.mediation.MediationAdRequest;
 import com.google.ads.mediation.customevent.CustomEventBanner;
 import com.google.ads.mediation.customevent.CustomEventBannerListener;
+import com.google.ads.mediation.customevent.CustomEventInterstitial;
+import com.google.ads.mediation.customevent.CustomEventInterstitialListener;
 
 /**
  * This class must be defined and referenced from AdMob's website for AdMob Mediation
@@ -19,39 +24,38 @@ import com.google.ads.mediation.customevent.CustomEventBannerListener;
  * @author David Anderson
  * @since 4.2
  */
-public class AdMobMediationListener implements CustomEventBanner
+public class AdMobMediationInterEvent implements CustomEventInterstitial
 { 
     
     /**
      * This method will be called by AdMob's Mediation through Custom Event mechanism.
      */
     @Override
-    public void requestBannerAd(final CustomEventBannerListener listener,
+    public void requestInterstitialAd(final CustomEventInterstitialListener listener,
             final Activity activity,
             String label,
             String serverParameter,
-            AdSize adSize,
             MediationAdRequest request)
     {
+        AppLovinInterstitialAdDialog dialog = AppLovinInterstitialAd.create(AppLovinSdk.getInstance(activity), activity);
 
-        // Create AppLovin Ad View
-        final AppLovinSdk sdk = AppLovinSdk.getInstance(activity);
-        final AppLovinAdView adView = new AppLovinAdView(sdk, AppLovinAdSize.BANNER, activity);
-        adView.setAdClickListener(new AppLovinAdClickListener() {
+        dialog.setAdDisplayListener(new AppLovinAdDisplayListener() {
             @Override
-            public void adClicked(AppLovinAd arg0)
+            public void adHidden(AppLovinAd ad)
             {
-                listener.onClick();
+                listener.onDismissScreen();
+            }
+
+            @Override
+            public void adDisplayed(AppLovinAd ad)
+            {
                 listener.onPresentScreen();
-                listener.onLeaveApplication();
             }
         });
 
-        // fetch an ad
-        sdk.getAdService().loadNextAd(AppLovinAdSize.BANNER, new AppLovinAdLoadListener() {
-
+        dialog.setAdLoadListener(new AppLovinAdLoadListener() {
             @Override
-            public void failedToReceiveAd(int errorCode)
+            public void failedToReceiveAd(int arg0)
             {
                 listener.onFailedToReceiveAd();
             }
@@ -59,9 +63,15 @@ public class AdMobMediationListener implements CustomEventBanner
             @Override
             public void adReceived(AppLovinAd ad)
             {
-                adView.renderAd(ad);
-                listener.onReceivedAd(adView);
+                listener.onReceivedAd();
             }
         });
+
+        dialog.show();
+    }
+
+    @Override
+    public void showInterstitial()
+    {        
     }
 }
